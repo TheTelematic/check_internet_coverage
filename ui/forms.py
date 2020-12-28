72f9b1c87ded9d620285f3223238d218d247b1c1
Arturo Parra <arturoteleco95@protonmail.ch>
2020-12-28 18:35:13 +0100

    [feature/#3]: Message and input field improved

--- ui/forms.py
+++ ui/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class AddressForm(forms.Form):
-    address = forms.CharField(max_length=500)
+    address = forms.CharField(max_length=500, widget=forms.TextInput(attrs={'class': 'address_input'}))
