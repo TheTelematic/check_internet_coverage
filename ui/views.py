06a0092958e36f4c63ac4f3ac5f477ddd8b079a3
Arturo Parra <arturoteleco95@protonmail.ch>
2020-12-28 16:34:42 +0100

    Hardcode address got it!

--- ui/views.py
+++ ui/views.py
@@ -23,9 +23,41 @@ def index(request):
         headers=dict(Referer='https://www.lowi.es/')
     )
 
-    return HttpResponse(
-        content=response.content,
-        status=response.status_code,
-        content_type=response.headers['Content-Type'],
+    response = session.post(
+        'https://www.lowi.es/consulta-cobertura/',
+        data={
+            'csrfmiddlewaretoken': csrf_token,
+            "name": "",
+            "email": "bar@bar.com",
+            "phone": "627654233",
+            "address": "Avenida+Plutarco,+69,+Málaga,+España",
+            "address_number": "69",
+            "check-privacy-policy": "on",
+            "legal_offer": "false",
+            "province": "MALAGA",
+            "province_id": "850000002",
+            "town": "MALAGA",
+            "town_id": "850000413",
+            "postal_code": "29010",
+            "stairwell": "",
+            "name_street": "PLUTARCO",
+            "number": "69",
+            "floor": "1",
+            "door": "B",
+            "horizontal": "8510078790",
+            "vertical": "851000000437236",
+            "street": "850010014",
+            "thoroughfare_type": "AV",
+            "error": "false",
+        },
+        allow_redirects=True,
+        headers=dict(Referer='https://www.lowi.es/consulta-cobertura/')
     )
+    print(response.url)
+    url = response.url
+    if url.endswith('/datos-titular/'):
+        message = 'Hay cobertura!'
+    else:
+        message = 'No hay cobertura :('
 
+    return render(request, 'ui/index.html', {'message': message})
