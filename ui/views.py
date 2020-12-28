d0cf07d2d5f9a2e225745a8917a160ccb0d16c84
Arturo Parra <arturoteleco95@protonmail.ch>
2020-12-28 16:51:33 +0100

    Simplify the workflow

--- ui/views.py
+++ ui/views.py
@@ -1,3 +1,5 @@
+from json import loads
+
 import requests
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -10,8 +12,7 @@ def index(request):
     response = session.get('https://www.lowi.es/')
     csrf_token = response.cookies['csrftoken']
 
-    print(f'CSRF Token: {csrf_token}')
-    response = session.post(
+    session.post(
         'https://www.lowi.es/configura-tu-compra/',
         data={
             'csrfmiddlewaretoken': csrf_token,
@@ -24,40 +25,36 @@ def index(request):
     )
 
     response = session.post(
-        'https://www.lowi.es/consulta-cobertura/',
+        'https://www.lowi.es/consulta-direccion/',
         data={
-            'csrfmiddlewaretoken': csrf_token,
             "name": "",
             "email": "bar@bar.com",
             "phone": "627654233",
             "address": "Avenida+Plutarco,+69,+Málaga,+España",
-            "address_number": "69",
-            "check-privacy-policy": "on",
-            "legal_offer": "false",
-            "province": "MALAGA",
-            "province_id": "850000002",
-            "town": "MALAGA",
-            "town_id": "850000413",
-            "postal_code": "29010",
-            "stairwell": "",
-            "name_street": "PLUTARCO",
-            "number": "69",
-            "floor": "1",
-            "door": "B",
-            "horizontal": "8510078790",
-            "vertical": "851000000437236",
-            "street": "850010014",
-            "thoroughfare_type": "AV",
-            "error": "false",
+            "province": "Málaga",
+            "provinceId": "850000002",
+            "town": "Málaga",
+            "townId": "850000413",
+            "street": "Plutarco",
+            "thoroughfareType": "",
+            "streetId": "",
+            "number": "69"
         },
         allow_redirects=True,
         headers=dict(Referer='https://www.lowi.es/consulta-cobertura/')
     )
-    print(response.url)
-    url = response.url
-    if url.endswith('/datos-titular/'):
+    response_dict = loads(response.content)
+
+    if response_dict['address'][0]['horizontals'][0]['id_horizontal'] != 0:
         message = 'Hay cobertura!'
     else:
         message = 'No hay cobertura :('
 
     return render(request, 'ui/index.html', {'message': message})
+
+    # return HttpResponse(
+    #     content=response.content,
+    #     status=response.status_code,
+    #     content_type=response.headers['Content-Type'],
+    # )
+
